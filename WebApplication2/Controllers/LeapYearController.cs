4326ece421a5c85130cc44b4075f7321003c3e75
tavisca-pkadam <tavisca-pakadam@tavisca.com>
2019-08-19 07:40:25 +0530

    Update Jenkins

--- WebApplication2/Controllers/LeapYearController.cs
+++ WebApplication2/Controllers/LeapYearController.cs
@@ -33,22 +33,7 @@ namespace WebApplication2
             return Json(new { nextYear = "2020" });
         }
 
-        // POST: LeapYear/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        
 
         // GET: LeapYear/Edit/5
         public ActionResult Edit(int id)
@@ -56,44 +41,7 @@ namespace WebApplication2
             return View();
         }
 
-        // POST: LeapYear/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: LeapYear/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: LeapYear/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        
+        
     }
 }
